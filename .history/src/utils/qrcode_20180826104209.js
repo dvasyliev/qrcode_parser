c82feebd6c8508e9762a58704122777f21670816
Dmytro Vasyliev <dvasyliev.dev@gmail.com>
2018-08-30 13:30:37 +0300

    Remove history

--- .history/src/utils/qrcode_20180826104209.js
+++
@@ -1,7 +0,0 @@
-const treeBuilder = (value) => {
-  const id = value.splice(0, 2)
-  const length = value.splice(2, 2)
-  const value = value.splice(4, length)
-
-  console.log(id, length, value)
-}
